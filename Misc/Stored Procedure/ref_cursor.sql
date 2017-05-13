b05584f8106536fb3863380d009dcf5293e244bb
Tyler D Muth <tmuth@splunk.com>
2017-05-13 18:51:20 -0400

    Created as examples

---
+++ Misc/Stored Procedure/ref_cursor.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE PROCEDURE ref_cur_test(
+  p_ref_cursor  OUT SYS_REFCURSOR, p_var_in IN VARCHAR)
+AS
+BEGIN
+ OPEN p_ref_cursor FOR
+ SELECT 'you passed-in: '|| p_var_in out_var FROM dual;
+END ref_cur_test;
+/
+-- test it from sqlplus / SQL Dev
+
+set serveroutput on
+DECLARE
+  l_cursor  SYS_REFCURSOR;
+  l_var   varchar2(255);
+BEGIN
+  ref_cur_test (p_var_in    => 'Foo',
+          p_ref_cursor => l_cursor);
+
+  LOOP
+    FETCH l_cursor
+    INTO  l_var;
+    EXIT WHEN l_cursor%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE(l_var);
+  END LOOP;
+  CLOSE l_cursor;
+END;
+/
+
+
+CREATE OR REPLACE PROCEDURE ref_cur_date_test(
+  p_ref_cursor  OUT SYS_REFCURSOR, p_var_in IN timestamp)
+AS
+BEGIN
+ OPEN p_ref_cursor FOR
+ SELECT 'you passed-in: '|| p_var_in out_var FROM dual;
+END ref_cur_date_test;
+/
