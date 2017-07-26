e8dbaa1b0890f672c210f1958e3b8ade20a11931
Tyler D Muth <tmuth@splunk.com>
2017-07-26 14:29:06 -0400

    created

---
+++ Security/Standard Audit/create-audit_reader-user.sql
@@ -0,0 +1,15 @@
+-- USER SQL
+CREATE USER audit_reader IDENTIFIED BY welcome1
+DEFAULT TABLESPACE "USERS"
+TEMPORARY TABLESPACE "TEMP";
+
+-- QUOTAS
+ALTER USER audit_reader QUOTA UNLIMITED ON USERS;
+
+-- ROLES
+GRANT "CONNECT" TO audit_reader ;
+
+-- SYSTEM PRIVILEGES
+GRANT CREATE SESSION TO audit_reader ;
+
+grant select on dba_audit_trail to audit_reader;
