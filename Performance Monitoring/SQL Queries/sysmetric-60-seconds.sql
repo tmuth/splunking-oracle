06bd0b5e96bf3e4f168856c761593b81fc1a3fdd
Tyler Muth <tylerdmuth-git@yahoo.com>
2019-09-23 14:16:11 -0400

    Moved end_time filter predicate as it was filtering CPU_COUNT too

--- Performance Monitoring/SQL Queries/sysmetric-60-seconds.sql
+++ Performance Monitoring/SQL Queries/sysmetric-60-seconds.sql
@@ -10,6 +10,7 @@ FROM
     METRIC_UNIT
   FROM sys.GV_$SYSMETRIC
   WHERE group_id   = 2
+  AND END_TIME > ?
   AND metric_name IN ('Host CPU Utilization (%)','CPU Usage Per Sec','Host CPU Usage Per Sec','Average Active Sessions','Database Time Per Sec', 'Executions Per Sec','Hard Parse Count Per Sec','Logical Reads Per Sec','Logons Per Sec', 'Physical Read Total Bytes Per Sec','Physical Read Total IO Requests Per Sec','Physical Reads Per Sec','Physical Write Total Bytes Per Sec', 'Redo Generated Per Sec','User Commits Per Sec','Current Logons Count','DB Block Gets Per Sec','DB Block Changes Per Sec', 'Database Wait Time Ratio','Database CPU Time Ratio','SQL Service Response Time','Background Time Per Sec', 'Physical Write Total IO Requests Per Sec','Physical Writes Per Sec','Physical Writes Direct Per Sec','Physical Writes Direct Lobs Per Sec', 'Physical Reads Direct Per Sec','Physical Reads Direct Lobs Per Sec', 'GC CR Block Received Per Second','GC Current Block Received Per Second','Global Cache Average CR Get Time','Global Cache Average Current Get Time', 'Global Cache Blocks Corrupted'
     ,'Global Cache Blocks Lost', 'Active Parallel Sessions','Active Serial Sessions','Average Synchronous Single-Block Read Latency', 'Response Time Per Txn','SQL Service Response Time' )
   UNION ALL
@@ -22,5 +23,4 @@ FROM
   WHERE name='cpu_count'
   GROUP BY inst_id
   )
-  WHERE END_TIME > ?
 ORDER BY END_TIME ASC
